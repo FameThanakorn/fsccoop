c72c471915a0a8058a5aae1069b8a628c19d32db
Thanakorn Tepsuwun <tarnfame01@gmail.com>
2020-06-08 17:58:57 +0700

    Fame

--- application/controllers/Nuk/PDF_02.php
+++ application/controllers/Nuk/PDF_02.php
@@ -15,23 +15,11 @@ class PDF_02 extends CI_Controller {
     public function Contract08()
 	{
 		// print_r($_GET['member_id']); exit;
-		$loan_id = '6482';
+		$loan_id = '9001';
 		$arr_data = array();
 		$this->load->model('Fsccoop_data', 'Fsccoop_data');
-		// $result = $this->Fsccoop_data->get_profile($loan_id);
-		// $arr_data["user"] = $result;
-		// $result2 = $this->Fsccoop_data->fscoop_getloan_amount($loan_id);
-		// $arr_data["loan_amount"] = $result2;
-		// $result3 = $this->Fsccoop_data->fscoop_province($loan_id);
-		// $arr_data["province"] = $result3;
-		// $result4 = $this->Fsccoop_data->fscoop_amphur($loan_id);
-		// $arr_data["amphur"] = $result4;
-		// $result5 = $this->Fsccoop_data->fscoop_district($loan_id);
-		// $arr_data["district"] = $result5;
-		// $result6 = $this->Fsccoop_data->fscoop_share($loan_id);
-		// $arr_data['share'] = $result6;
 		$result = $this->Fsccoop_data->fscoop_full($loan_id);
-		$arr_data['fscoop_full'] = $result;
+		$arr_data['loan_fscoop'] = $result;
 
 		// echo "<pre>";
 		// print_r($arr_data);
@@ -39,6 +27,7 @@ class PDF_02 extends CI_Controller {
 		$this->load->library('Center_function');
 		$test = $this->center_function->convert('5');
 		$this->center_function->mydate2date($row_member['birthday']);
+		// echo $test;exit;
 		
         $this->load->view('PDF_01/Contract08',$arr_data);
 	}
--- application/models/Fsccoop_data.php
+++ application/models/Fsccoop_data.php
@@ -81,9 +81,13 @@ class Fsccoop_data extends CI_Model
 		$this->db->join("coop_district as N4","N2.district_id = N4.district_id","left");
 		$this->db->join("coop_amphur as N5","N2.amphur_id = N5.amphur_id","left");
 		$this->db->join("coop_province as N6","N2.province_id = N6.province_id","left");
+		// $this->db->join("coop_province as N9","N2.c_province_id = N9.province_id","left");
+		$this->db->join("coop_prename as N7","N2.prename_id = N7.prename_id","left");
+		$this->db->join("coop_mem_group as N8","N2.level = N8.id","left");
 		$this->db->WHERE("N1.id = '$loan_id'");
 		$row = $this->db->get()->row_array();
 		$arr_data['full'] =  $row;
+		// echo '<pre>';print_r($row); exit;
 		return $arr_data['full'];
 	}
 
